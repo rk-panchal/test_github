f4a4fe2876b520c39a207d3c7bdc82db6a3bad94
rk-panchal <kala2437@gmail.com>
2018-03-21 12:14:16 +0530

    routs for comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180321061945) do
+ActiveRecord::Schema.define(version: 20180321064054) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "Name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
