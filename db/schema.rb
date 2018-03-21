b6be8bc89967e6919418e973ff0506d74ef337e9
rk-panchal <kala2437@gmail.com>
2018-03-21 12:09:34 +0530

    merge dev into master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180106062727) do
+ActiveRecord::Schema.define(version: 20180321061945) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
